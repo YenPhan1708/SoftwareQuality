7bac596c69146075d3795f8ffaf718b5991a93af
YenPhan1708 <yen.phan@student.nhlstenden.com>
2025-03-31 00:24:47 +0200

    Design Pattern Applied

--- Jabberpoint-IT-main/src/SlideItemInterface.java
+++
@@ -1,9 +0,0 @@
-import java.awt.*;
-import java.awt.image.ImageObserver;
-
-public interface SlideItemInterface
-{
-    int getLevel();
-    Rectangle getBoundingBox(Graphics g, ImageObserver observer, float scale, Style style);
-    void draw(int x, int y, float scale, Graphics g, Style style, ImageObserver observer);
-}
