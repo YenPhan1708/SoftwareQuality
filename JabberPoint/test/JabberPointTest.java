e74984061738bec71902336d4688623450e8b490
Cbo Huang <cbo.huang@student.nhlstenden.com>
2025-03-31 21:03:54 +0200

    implemented extra test case for JabberPointTEst
    implemented extra test case for JabberPointTest

--- JabberPoint/test/JabberPointTest.java
+++ JabberPoint/test/JabberPointTest.java
@@ -51,4 +51,44 @@ public class JabberPointTest {
             mainMethod.invoke(null, (Object) args);
         });
     }
+
+    @Test
+    public void testCreateAccessorAlwaysReturnsDemoPresentation() {
+        JabberPoint app = new JabberPoint(new Presentation());
+        for (int i = 0; i < 5; i++) {
+            assertTrue(app.createAccessor() instanceof DemoPresentation);
+        }
+    }
+
+    @Test
+    public void testMainWithNullArgsDoesNotCrash() throws Exception {
+        Method main = JabberPoint.class.getMethod("main", String[].class);
+        assertDoesNotThrow(() -> main.invoke(null, (Object) null));
+    }
+
+    @Test
+    public void testMainWithEmptyArgString() throws Exception {
+        Method main = JabberPoint.class.getMethod("main", String[].class);
+        String[] args = {""};
+        assertDoesNotThrow(() -> main.invoke(null, (Object) args));
+    }
+
+    @Test
+    public void testMainWithMultipleArgs() throws Exception {
+        Method main = JabberPoint.class.getMethod("main", String[].class);
+        String[] args = {"file1.xml", "extra.xml"};
+        assertDoesNotThrow(() -> main.invoke(null, (Object) args));
+    }
+
+    @Test
+    public void testMainWithFakeFileTriggersIOExceptionHandling() throws Exception {
+        Method main = JabberPoint.class.getMethod("main", String[].class);
+        String[] args = {"nonexistent-file.xml"};
+        assertDoesNotThrow(() -> main.invoke(null, (Object) args));  // should catch and print stack trace internally
+    }
+
+    @Test
+    public void testConstructorWithNullPresentationDoesNotThrow() {
+        assertDoesNotThrow(() -> new JabberPoint(null));
+    }
 }
