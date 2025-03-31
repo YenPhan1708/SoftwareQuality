52e9367e599196ead783e7f93a3aafffdfe8f3b9
Cbo Huang <cbo.huang@student.nhlstenden.com>
2025-03-31 20:24:42 +0200

    improve AboutBoxTest
    added more test cases for AboutBoxTest

--- JabberPoint/test/AboutBoxTest.java
+++ JabberPoint/test/AboutBoxTest.java
@@ -17,4 +17,36 @@ public class AboutBoxTest {
 
         frame.dispose();
     }
+
+    @Test
+    public void testShowWithNullParent() {
+        assertDoesNotThrow(() -> AboutBox.show(null), "Should not throw when parent is null");
+    }
+
+    @Test
+    public void testShowWithMinimalFrame() {
+        Frame frame = new Frame();
+        assertDoesNotThrow(() -> AboutBox.show(frame), "Should show AboutBox with empty frame");
+    }
+
+    @Test
+    public void testShowWithConfiguredFrame() {
+        Frame frame = new Frame("Main Frame");
+        frame.setSize(400, 300);
+        assertDoesNotThrow(() -> AboutBox.show(frame), "Should show AboutBox with sized frame");
+    }
+
+    @Test
+    public void testShowWhenCalledMultipleTimes() {
+        Frame frame = new Frame();
+        assertDoesNotThrow(() -> AboutBox.show(frame));
+        assertDoesNotThrow(() -> AboutBox.show(frame));
+    }
+
+    @Test
+    public void testShowFromAlreadyVisibleFrame() {
+        Frame frame = new Frame("Visible Frame");
+        frame.setVisible(true);
+        assertDoesNotThrow(() -> AboutBox.show(frame));
+    }
 }
